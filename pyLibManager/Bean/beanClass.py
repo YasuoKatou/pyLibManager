600734f48e9a92b021aa548ad90e29735a450470
加藤康夫 <yasuokatou@gmail.com>
2022-01-30 21:55:45 +0900

    Beanコピー時の変換（日付）、整数数値の変換）を実装

--- pyLibManager/Bean/beanClass.py
+++ pyLibManager/Bean/beanClass.py
@@ -4,6 +4,10 @@ class XNoSupportedBeanMapTypeError(Exception):
     def __init__(self, typeList):
         super().__init__('map type error (%s)' % typeList)
 
+class XNoSupportedConvertTypeError(Exception):
+    def __init__(self, convertType):
+        super().__init__('convert type error (%s)' % convertType)
+
 class BeanCopy:
 
     def copy(self, fromBean, toBean, map):
@@ -16,17 +20,29 @@ class BeanCopy:
         '''
             map param: [
                 {'fromTo': ['from key', 'to key'],
-                 'convert': {'type': 'date', 'format': 'yyyy/mm/dd'}
+                 'convert': {'type': 'DateTimeString', 'format': '%Y-%m-%d %H:%M:%S'}
                 }
             ]
             copy     : toBean['tk1'] = fromBean['fk1']
         '''
         for map in mapList:
-            for k, v in map.items():
-                ft = map['fromTo']
-                toBean[ft[1]] = None if ft[0] not in fromBean else fromBean[ft[0]]
+            ft = map['fromTo']
+            v = None if ft[0] not in fromBean else fromBean[ft[0]]
+            if v and ('convert' in map):
+                c = map['convert']
+                if c['type'] == 'DateTimeString':
+                    v = v.strftime(c['format'])
+                elif c['type'] == 'Int2String':
+                    v = str(v)
+                elif c['type'] == 'String2Int':
+                    v = int(v)
+                else:
+                    raise XNoSupportedConvertTypeError(c['type'])
+
+            toBean[ft[1]] = v
 
 if __name__ == '__main__':
+    from datetime import datetime as DT
     print('unit test start')
     tc = BeanCopy()
 
@@ -45,6 +61,22 @@ if __name__ == '__main__':
     assert len(t) == 1, '[%s] コピー先が空' % (tn, )
     assert 'key2' in t, '[%s] key2 に key1 の内容がコピーされていない' % (tn, )
     assert t['key2'] == 'value1', '[%s] コピー先の値が不正 (%s)' % (tn, str(t['key2']))
+
+    tn = 'tesst case 3'
+    f = {'key1': DT.fromisoformat('2022-01-30 15:34:23'), 'key2': 123, 'key3': '456'}
+    t = {}
+    copyMap = [{'fromTo':['key1', 'key1'], 'convert': {'type': 'DateTimeString', 'format': '%Y/%m/%d %H:%M:%S'}},
+               {'fromTo':['key2', 'key2-1'], 'convert': {'type': 'Int2String'}},
+               {'fromTo':['key2', 'key2-2']},
+               {'fromTo':['key3', 'key3'], 'convert': {'type': 'String2Int'}}]
+    tc.copy(f, t, copyMap)
+    assert len(f) == 3, '[%s] コピー元が空' % (tn, )
+    assert len(t) == 4, '[%s] コピーがただしくない' % (tn, )
+    assert 'key1' in t, '[%s] key1 に key1 の内容がコピーされていない' % (tn, )
+    assert t['key1'] == '2022/01/30 15:34:23', '[%s] コピー先の値が不正 (%s)' % (tn, str(t['key1']))
+    assert t['key2-1'] == '123', '[%s] コピー先の値が不正 (%s)' % (tn, str(t['key2-1']))
+    assert t['key2-2'] == 123, '[%s] コピー先の値が不正 (%s)' % (tn, str(t['key2-2']))
+    assert t['key3'] == 456, '[%s] コピー先の値が不正 (%s)' % (tn, str(t['key3']))
 
     print('unit test end')
 
