59bc13316771a9c765d94baa9f953b74be3f24e1
加藤康夫 <yasuokatou@gmail.com>
2022-03-12 17:13:52 +0900

    コネクションプールの実装

--- pyLibManager/DBs/PostgreSQL/pgClass.py
+++ pyLibManager/DBs/PostgreSQL/pgClass.py
@@ -5,8 +5,15 @@ import logging.config
 import os
 import psycopg2
 import psycopg2.extras
+#TODO 本来、コメントアウトした定義にしたいが、pyLibManagerでThreadedConnectionPoolを
+#     ロードしようとしてエラーとなるためfromなしのimportで回避している.
+#from psycopg2.pool import ThreadedConnectionPool
+import psycopg2.pool
 
 class PG:
+    def __init__(self):
+        self._conn_pool = None
+
     def setLogConfig(self, log_conf):
         logging.config.dictConfig(log_conf)
         self.logger = logging.getLogger(__name__)
@@ -26,6 +33,18 @@ class PG:
         dns = os.environ.get(env_name)
         assert dns, '環境変数[%s] (PostgreSQL接続文字列) が設定されていません.' % (env_name, )
         return dns
+
+    def setConnectionPool(self, dbInfo):
+        dns = self.getDnsByEnv(dbInfo['env_name'])
+        self._conn_pool = psycopg2.pool.ThreadedConnectionPool(dbInfo['pool']['min'], dbInfo['pool']['max'], dns)
+
+    def getConn(self):
+        assert self._conn_pool, '[getConn] コネクションプールが初期化されていません.'
+        return self._conn_pool.getconn()
+
+    def putConn(self, conn):
+        assert self._conn_pool, '[putConn] コネクションプールが初期化されていません.'
+        self._conn_pool.putconn(conn)
 
     def connect(self):
         return psycopg2.connect(self.dns)
