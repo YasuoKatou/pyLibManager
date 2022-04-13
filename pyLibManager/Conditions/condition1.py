39524e7ba79a251a916d0bb91030f64bf60a8896
加藤康夫 <yasuokatou@gmail.com>
2022-04-13 20:05:17 +0900

    パスの変更

--- pyLibManager/Conditions/condition1.py
+++ pyLibManager/Conditions/condition1.py
@@ -31,6 +31,12 @@ class Condition1:
                     return False
         return False
 
+    def isEmpty(self, val):
+        return False if val else True
+
+    def isNotEmpty(self, val):
+        return not self.isEmpty(val)
+
 if __name__ == '__main__':
     c = Condition1()
 
@@ -52,4 +58,17 @@ if __name__ == '__main__':
     assert c.isOne('1.00e-0'), '文字["1.00e-0"]が正しく判定されていない.'
     assert c.isOne('0x01'), '文字["0x01"]が正しく判定されていない.'
 
+    assert c.isEmpty(0), '数値[0]が空でないと判定された.'
+    assert c.isEmpty(1) == False, '数値[1]が空と判定された.'
+    assert c.isEmpty(''), '空文字列[""]が空でないと判定された.'
+    assert c.isEmpty('1') == False, '文字列["1"]が空と判定された.'
+    assert c.isEmpty(None), 'Noneが空でないと判定された.'
+
+    assert c.isNotEmpty(0) == False, '数値[0]が空と判定された.'
+    assert c.isNotEmpty(1), '数値[1]が空でないと判定された.'
+    assert c.isNotEmpty('') == False, '空文字列[""]が空と判定された.'
+    assert c.isNotEmpty('1'), '文字列["1"]が空でないと判定された.'
+    assert c.isNotEmpty(None) == False, 'Noneが空と判定された.'
+
+    print('test is all OK !!')
 #[EOF]
